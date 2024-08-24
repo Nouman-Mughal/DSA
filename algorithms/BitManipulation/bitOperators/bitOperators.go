3915b4fc967bb80b3a47d8c81de88437e95ce456
noman-reborn <118903496+noman-reborn@users.noreply.github.com>
2024-08-24 21:46:35 +0500

    feat: bitOperators

--- algorithms/BitManipulation/bitOperators/bitOperators.go
+++ algorithms/BitManipulation/bitOperators/bitOperators.go
@@ -32,7 +32,23 @@ func bitwiseXOR(a int, b int) int {
 
 // Bitwise compliment operator is an unary operator (works on only one operand). It changes 1 to 0 and 0 to 1.
 // Go doesnt have ~ operator like C or other languages but we can do it using xor operator.
+// bitwise compliment of signed Int is -(N +1) and for signed int its just (N + 1)
 
 func bitwiseComplement(a int) int {
 	return ^a
+}
+
+/*
+*
+Right shift operator shifts all bits towards right by certain number of specified bits.
+If we’re starting with a negative number (a binary number where the leftmost bit is a 1),all the empty spaces are filled with a 1.
+If we’re starting with a positive number (where the leftmost bit, or most significant bit, is a 0),
+We do all this to preserve the sign.
+then all the empty spaces are filled with a 0.
+*/
+func bitwiseRightShift(a int, n int) int {
+
+	// Right shift by N.
+	// ! right shift turns unsigned int into signed int.
+	return a >> n
 }
